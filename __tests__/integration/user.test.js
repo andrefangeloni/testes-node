7f10911cbcd113a02461f68ea8cfb5abc77d53c5
andrefangeloni <andrefangeloni@gmail.com>
2019-08-28 23:14:45 -0300

    Verificando se existe e-mail duplicado

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,12 @@
 import request from 'supertest';
 import app from '../../src/app';
+import truncate from '../util/truncate';
 
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -11,5 +16,25 @@ describe('User', () => {
         password_hash: '123456',
       });
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'diego@rocketseat.com.br',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'diego@rocketseat.com.br',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
