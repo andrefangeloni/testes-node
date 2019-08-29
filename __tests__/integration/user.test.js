653d4c57fc894b97ce84a7d88a13ecbff8ca3f11
andrefangeloni <andrefangeloni@gmail.com>
2019-08-28 22:56:14 -0300

    Teste de criação de usuário

---
+++ __tests__/integration/user.test.js
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import app from '../../src/app';
+
+describe('User', () => {
+  it('should be able to register', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'diego@rocketseat.com.br',
+        password_hash: '123456',
+      });
+    expect(response.body).toHaveProperty('id');
+  });
+});
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,3 +1,5 @@
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 
-dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
+module.exports = dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
